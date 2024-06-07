42732d471643dfb6f95ef494061d8df6b89a7d94
sankar prasad biswas <sankarbiswas07@gmail.com>
2024-06-07 18:54:46 +0530

    graphql query by id

---
+++ src/__mocks__/mockUser.ts
@@ -0,0 +1,103 @@
+export const mockUsers = [
+    {
+      id: 1,
+      username: 'ram',
+      displayName: 'Ram The Dancer',
+    },
+    {
+      id: 2,
+      username: 'Tipu',
+      displayName: 'Tipu The Thinker',
+    },
+    {
+      id: 3,
+      username: 'peter',
+      displayName: 'Peter The Developer',
+    },
+    {
+      id: 4,
+      username: 'lucy',
+      displayName: 'Lucy The Coder',
+    },
+    {
+      id: 5,
+      username: 'ethan',
+      displayName: 'Ethan The Programmer',
+    },
+    {
+      id: 6,
+      username: 'mia',
+      displayName: 'Mia The Architect',
+    },
+    {
+      id: 7,
+      username: 'oliver',
+      displayName: 'Oliver The Analyst',
+    },
+    {
+      id: 8,
+      username: 'sophia',
+      displayName: 'Sophia The Engineer',
+    },
+    {
+      id: 9,
+      username: 'james',
+      displayName: 'James The Developer',
+    },
+    {
+      id: 10,
+      username: 'ava',
+      displayName: 'Ava The Specialist',
+    },
+    {
+      id: 11,
+      username: 'liam',
+      displayName: 'Liam The Designer',
+    },
+    {
+      id: 12,
+      username: 'emma',
+      displayName: 'Emma The Analyst',
+    },
+    {
+      id: 13,
+      username: 'noah',
+      displayName: 'Noah The Coder',
+    },
+    {
+      id: 14,
+      username: 'amelia',
+      displayName: 'Amelia The Developer',
+    },
+    {
+      id: 15,
+      username: 'olivia',
+      displayName: 'Olivia The Architect',
+    },
+    {
+      id: 16,
+      username: 'elijah',
+      displayName: 'Elijah The Programmer',
+    },
+    {
+      id: 17,
+      username: 'harper',
+      displayName: 'Harper The Specialist',
+    },
+    {
+      id: 18,
+      username: 'benjamin',
+      displayName: 'Benjamin The Engineer',
+    },
+    {
+      id: 19,
+      username: 'isabella',
+      displayName: 'Isabella The Designer',
+    },
+    {
+      id: 20,
+      username: 'lucas',
+      displayName: 'Lucas The Analyst',
+    },
+  ];
+  
--- src/graphql/resolvers/UserResolver.ts
+++ src/graphql/resolvers/UserResolver.ts
@@ -1,17 +1,19 @@
-import { Resolver, Query } from "@nestjs/graphql";
+import { Resolver, Query, Args, Int } from "@nestjs/graphql";
 import { User } from "../models/User";
+import { mockUsers } from "src/__mocks__/mockUser";
 
 
 @Resolver()
 export class UserResolver {
 
-    @Query((returns) => User)
-    getUsers(){
-        return {
-            id:1,
-            username: 'john',
-            displayName : 'j.doe'
-        }
+    @Query(() => [User])
+    getAllUsers() {
+        return mockUsers
+    }
+
+    @Query((returns) => User, { nullable: true })
+    getUser(@Args('id', { type: () => Int }) id: number) {
+        return mockUsers.find(user => user.id === id)
     }
 
 }
