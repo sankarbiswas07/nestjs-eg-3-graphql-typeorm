a6deff6cd7f00dd87e73f5fa8a81dfff3fbe25de
sankar prasad biswas <sankarbiswas07@gmail.com>
2024-06-08 22:13:49 +0530

    mysql connection in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,23 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UserResolver } from './graphql/resolvers/UserResolver';
 import { UserSettingResolver } from './graphql/resolvers/UserSettingResolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'src/schema.gql'
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'myuser',
+      password: 'mypassword',
+      database: 'nest_mysql',
+      synchronize: true,
+      entities: []
     })
   ],
   controllers: [],
